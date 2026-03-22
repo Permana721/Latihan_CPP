6f3a3c0d0ebc35dadc69548bc970e9c17e6aa023
Permana <sdharma721@gmail.com>
2026-03-22 21:01:20 +0700

    feat(2_pointer): implemented maximum number finder in Test5.cpp

--- _2_Pointer/Test5.cpp
+++ _2_Pointer/Test5.cpp
@@ -1,3 +1,20 @@
-//
-// Created by permana on 3/22/26.
-//
+#include <iostream>
+
+int main() {
+    int *array = new int[3];
+
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Masukan angka ke " << i+1 << ": ";
+        std::cin >> *(array + i);
+    }
+
+    int max_num = array[0];
+    for (int i = 0; i < 3; i++) {
+        if (*(array + i) > max_num) {
+            max_num = *(array + i);
+        }
+    }
+
+    std::cout << "angka terbesar adalah " << max_num;
+    delete[] array;
+}
