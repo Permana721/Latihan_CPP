dde4885d39c73b74d1cfc115eba11f10b892d5a7
Permana <sdharma721@gmail.com>
2026-03-22 21:01:18 +0700

    refactor(2_pointer): implements basic input/output functionality for arrays

--- _2_Pointer/Test4.cpp
+++ _2_Pointer/Test4.cpp
@@ -1,3 +1,17 @@
-//
-// Created by permana on 3/22/26.
-//
+#include <iostream>
+
+int main() {
+    int *array = new int[3];
+
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Masukan angka ke " << i+1 << ": ";
+        std::cin >> *(array + i);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Angka ke " << i+1 << " adalah: " << *(array + i) << std::endl;
+    }
+
+    delete[] array;
+    return 0;
+}
