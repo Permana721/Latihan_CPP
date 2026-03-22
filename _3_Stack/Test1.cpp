fcc25308015f3e7da61a38424457eafb22ff5e3a
Permana <sdharma721@gmail.com>
2026-03-23 00:34:01 +0700

    refactor(stack): introduce error handling for stack overflow/underflow in Stack class implementation

--- _3_Stack/Test1.cpp
+++ _3_Stack/Test1.cpp
@@ -1,3 +1,68 @@
-//
-// Created by permana on 3/23/26.
-//
+#include <iostream>
+using namespace std;
+
+#define MAX 5
+
+class Stack {
+private:
+    int arr[MAX];
+    int top;
+
+public:
+    Stack() {
+        top = -1;
+    }
+
+    bool isFull() {
+        return top == MAX - 1;
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    void push(int value) {
+        if (isFull()) {
+            cout << "Stack Overflow!\n";
+            return;
+        }
+        arr[++top] = value;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack Underflow!\n";
+            return;
+        }
+        top--;
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            cout << "Stack kosong!\n";
+            return -1;
+        }
+        return arr[top];
+    }
+
+    void display() {
+        for (int i = top; i >= 0; i--) {
+            cout << arr[i] << endl;
+        }
+    }
+};
+
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.display();
+
+    cout << "Top: " << s.peek() << endl;
+
+    s.pop();
+    s.display();
+}
