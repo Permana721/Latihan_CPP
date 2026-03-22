00160f844fe3739d159f8a8076a7ad80c9da67bf
Permana <sdharma721@gmail.com>
2026-03-22 21:01:22 +0700

    feat(2_pointer): add test for 3-number sum calculation

--- _2_Pointer/Test6.cpp
+++ _2_Pointer/Test6.cpp
@@ -1,3 +1,18 @@
-//
-// Created by permana on 3/22/26.
-//
+#include <iostream>
+
+int main() {
+    int *array = new int[3];
+
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Masukan angka ke " << i+1 << ": ";
+        std::cin >> *(array + i);
+    }
+
+    int hasil = 0;
+    for (int i = 0; i < 3; i++) {
+       hasil += *(array + i);
+    }
+
+    std::cout << "Hasil penjumlahan dari ketiga angka adalah: " << hasil;
+    delete[] array;
+}
