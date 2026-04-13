c975a76b183c2a7dbc88888632dc0b7fe4ccf240
Permana <sdharma721@gmail.com>
2026-04-13 15:40:27 +0700

    feat(Siswa2): Implemented data structure for storing student info, input/output functionality added

--- _1_Struct/_2_Siswa/Siswa2.cpp
+++ _1_Struct/_2_Siswa/Siswa2.cpp
@@ -1,3 +1,30 @@
-//
-// Created by permana on 4/13/26.
-//
+#include <iostream>
+using namespace std;
+
+typedef struct {
+    string nama;
+    int umur;
+    float nilai;
+} Siswa;
+
+int main() {
+    Siswa siswa[5];
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Masukkan nama untuk siswa ke " << i+1 << ": ";
+        std::cin >> siswa[i].nama;
+        std::cout << "Masukkan umur untuk siswa ke " << i+1 << ": ";
+        std::cin >> siswa[i].umur;
+        std::cout << "Masukkan nilai untuk siswa ke " << i+1 << ": ";
+        std::cin >> siswa[i].nilai;
+        std::cout << "\n";
+    }
+    std::cout << "======================================================";
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "\nNama siswa ke " << i+1 << " adalah: " << siswa[i].nama;
+        std::cout << "\nUmur siswa ke " << i+1 << " adalah: " << siswa[i].umur;
+        std::cout << "\nNilai siswa ke " << i+1 << " adalah: " << siswa[i].nilai;
+        std::cout << "\n";
+    }
+}
