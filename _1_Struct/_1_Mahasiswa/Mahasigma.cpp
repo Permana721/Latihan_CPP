e2aec62fd6191fe13e18fca1a182ded0716624fb
Permana <sdharma721@gmail.com>
2026-04-13 15:40:15 +0700

    `feat(Struct/Mahasiswa): Implement Mahasigma structure and main function`

--- _1_Struct/_1_Mahasiswa/Mahasigma.cpp
+++ _1_Struct/_1_Mahasiswa/Mahasigma.cpp
@@ -1,3 +1,19 @@
-//
-// Created by permana on 4/13/26.
-//
+#include <iostream>
+using namespace std;
+
+typedef struct {
+    string nama;
+    int umur;
+    float ipk;
+} Mahasigma;
+
+int main() {
+    Mahasigma mhs;
+
+    mhs.nama = "Permana Surya Dharma";
+    mhs.umur = 18;
+    mhs.ipk = 4.12;
+    cout << "Nama anda adalah: " << mhs.nama;
+    cout << "\nUmur anda adalah: " << mhs.umur;
+    cout << "\nIPK Anda adalah: " << mhs.ipk;
+}
