9ea68abec4bcfe49602d582ea98c8b76f35291c0
Permana <sdharma721@gmail.com>
2026-04-15 20:10:19 +0700

    refactor(2_Pointer): introduce pointer usage in Contoh2.cpp

--- _2_Pointer/Contoh2.cpp
+++ _2_Pointer/Contoh2.cpp
@@ -1,3 +1,11 @@
-//
-// Created by permana on 4/13/26.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    float y, *x1, *x2;
+    y = 12.34;
+    x1 = &y;
+    x2 = x1;
+    std::cout << "nilai Y yang di tunjuk oleh x1 adalah " << *x1 << " di alamat " << x1 << endl;
+    std::cout << "nilai Y yang di tunjuk oleh x2 adalah " << *x2 << " di alamat " << x2;
+}
