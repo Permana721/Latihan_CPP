64803e839c635876612a67b1af4bb245e0ac3e3b
Permana <sdharma721@gmail.com>
2026-04-15 20:10:23 +0700

    feat(2_Pointer): Implement basic 2-pointer algorithm in Contoh4.cpp

--- _2_Pointer/Contoh4.cpp
+++ _2_Pointer/Contoh4.cpp
@@ -1,3 +1,11 @@
-//
-// Created by permana on 4/14/26.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 25, b = 12;
+    int *p, *q;
+    p = &a;
+    q = &b;
+    std::cout << "Nilai a adalah " << a << " dan disimpan pada memori " << p << endl;
+    std::cout << "Nilai b adalah " << b << " dan disimpan pada memori " << q;
+}
